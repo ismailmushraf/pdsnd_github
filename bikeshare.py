ff253aa1a59b8ca0c812a98bc1f585f5c09bc9e4
ismailmushraf <ismailmushraf@gmail.com>
2023-07-09 19:37:43 +0530

    code_desc: Changed user_stats method description

--- bikeshare.py
+++ bikeshare.py
@@ -149,7 +149,7 @@ def trip_duration_stats(df):
 
 
 def user_stats(df):
-    """Displays statistics on bikeshare users."""
+    """Displays statistics on bikeshare users based on Gender."""
     try:
         print('\nCalculating User Stats...\n')
         start_time = time.time()
